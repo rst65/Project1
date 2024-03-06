e5e590cda2b5efdcfe314114cdac73758163eb4f
rst65 <157853263+rst65@users.noreply.github.com>
2024-03-05 21:47:06 -0600

    Update Server_tcp.c

--- Server_tcp.c
+++ Server_tcp.c
@@ -5,100 +5,10 @@
 #include <netinet/in.h>
 #include <string.h>
 
-#define MAX_STUDENTS 100
-
-struct Student {
-    int ID;
-    char Fname[10];
-    char Lname[10];
-    float score; };
-
-struct Student students[MAX_STUDENTS];
-int numStudents = 0;
-
-void add(int socket) {
-    if (numStudents >= MAX_STUDENTS) {
-        printf("Database is full.\n");
-        return;
-    }
-
-    struct Student newStudent;
-    printf("Enter student ID: ");
-    scanf("%d", &newStudent.ID);
-    printf("Enter first name: ");
-    scanf("%s", newStudent.Fname);
-    printf("Enter last name: ");
-    scanf("%s", newStudent.Lname);
-    printf("Enter score: ");
-    scanf("%f", &newStudent.score);
-
-    students[numStudents] = newStudent;
-    numStudents++;
-    printf("Student added successfully!\n");
-}
-
-// Function to display a student's information by ID
-void display(int socket) {
-    int searchID;
-    printf("Enter student ID to display: ");
-    scanf("%d", &searchID);
-
-    for (int i = 0; i < numStudents; i++) {
-        if (students[i].ID == searchID) {
-            printf("Student ID: %d\n", students[i].ID);
-            printf("Name: %s %s\n", students[i].Fname, students[i].Lname);
-            printf("Score: %.2f\n", students[i].score);
-            return;
-        }
-    }
-    printf("Student with ID %d not found.\n", searchID);
-}
-
-// Function to display information of students with scores above a given value
-void display_score(int socket) {
-    float minScore;
-    printf("Enter minimum score: ");
-    scanf("%f", &minScore);
-
-    printf("Students with scores above %.2f:\n", minScore);
-    for (int i = 0; i < numStudents; i++) {
-        if (students[i].score > minScore) {
-            printf("ID: %d, Name: %s %s, Score: %.2f\n",
-                   students[i].ID, students[i].Fname, students[i].Lname, students[i].score);
-        }
-    }
-}
-
-// Function to display information of all students
-void display_all(int socket) {
-    printf("All students in the database:\n");
-    for (int i = 0; i < numStudents; i++) {
-        printf("ID: %d, Name: %s %s, Score: %.2f\n",
-               students[i].ID, students[i].Fname, students[i].Lname, students[i].score);
-    }
-}
-
-// Function to delete a student by ID
-void delete(int socket) {
-    int deleteID;
-    printf("Enter student ID to delete: ");
-    scanf("%d", &deleteID);
-
-    for (int i = 0; i < numStudents; i++) {
-        if (students[i].ID == deleteID) {
-            // Shift remaining elements to fill the gap
-            for (int j = i; j < numStudents - 1; j++) {
-                students[j] = students[j + 1];
-            }
-            numStudents--;
-            printf("Student with ID %d deleted successfully!\n", deleteID);
-            return;
-        }
-    }
-    printf("Student with ID %d not found.\n", deleteID);
-}
-
-
+void add(int newSocket){
+recv(newSocket, &ID, sizeof(ID), 0);
+                ID = ntohl(ID);
+printf(ID);}
 
 int main(int argc, char **argv){
 int welcomeSocket, newSocket;
@@ -133,8 +43,6 @@ if(listen(welcomeSocket,5)==0)
 printf("Listening\n");
 else
 printf("Error\n");
-
-
     while(1) {
 
             addr_size = sizeof serverStorage;
@@ -166,7 +74,7 @@ newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage,
             } while (choice != 6);
 
             close(newSocket);
-            exit(0);
+            exit(0); 
         }
 
         // Parent process closes the new socket and goes back to listening
